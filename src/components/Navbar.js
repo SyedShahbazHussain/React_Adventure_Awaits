4ee432371facbd7b79696bf01160cc8c95a34a02
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2021-03-22 23:09:31 +0500

    Making btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React ,{useState} from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import { Link } from "react-router-dom"
 
 function Navbar() {
     const [click, setClick] = useState(false)
-    const handleClick =()=> setClick(!click)
-    const closeMobileMenu =()=> setClick(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
     return (
         <>
             <nav className="navbar">
@@ -13,9 +13,9 @@ function Navbar() {
                         TRVL <i className="fab fa-typo3" />
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times":"fas fa-bars"} />
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
                     </div>
-                    <ul className={click?'nav-menu active':"nav-menu"}>
+                    <ul className={click ? 'nav-menu active' : "nav-menu"}>
                         <li className="nav-item">
                             <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                                 Home</Link>
