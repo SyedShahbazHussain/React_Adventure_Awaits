197f4da30f3646f5370db8676b3dc43f04d9637c
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2021-04-26 00:58:33 +0500

    add side bar

--- src/components/Button.js
+++ src/components/Button.js
@@ -25,7 +25,6 @@ export const Button =({
             >
                 {children}
             </button>
-            <Button>GET Stated</Button>
         </Link>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
-
+import { Button } from './Button'
+import './Navbar.css'
 function Navbar() {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton)
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         TRVL <i className="fab fa-typo3" />
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
@@ -33,6 +48,7 @@ function Navbar() {
                                 Sign Up</Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
                 </div>
             </nav>
         </>
